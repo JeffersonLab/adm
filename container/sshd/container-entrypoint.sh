c57ecc45548bd26848f3f6bf1ae6012c85d2a21b
slominskir <ryan.slominski@gmail.com>
2024-03-06 10:01:53 -0500

    Initial commits

---
+++ container/sshd/container-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -n "${TEST_USER_AUTHORIZED_KEY}" ]; then
+  echo "Populating /testuser/.ssh/authorized_keys with the value from TEST_USER_AUTHORIZED_KEY env variable ..."
+  echo "${TEST_USER_AUTHORIZED_KEY}" > /home/testuser/.ssh/authorized_keys
+fi
+
+/usr/sbin/sshd -e -D
+
+sleep infinity
