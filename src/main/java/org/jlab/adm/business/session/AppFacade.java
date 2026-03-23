7fe08f69b497a9fb8e325751c47b6f1c296b8e7f
ryans <ryans@jlab.org>
2026-03-23 10:43:03 -0400

    Add CRUD
    Fixes #9

--- src/main/java/org/jlab/adm/business/session/AppFacade.java
+++ src/main/java/org/jlab/adm/business/session/AppFacade.java
@@ -1,9 +1,13 @@
 package org.jlab.adm.business.session;
 
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.jlab.adm.persistence.entity.App;
+import org.jlab.smoothness.business.exception.UserFriendlyException;
+
+import java.math.BigInteger;
 
 @Stateless
 public class AppFacade extends AbstractFacade<App> {
@@ -17,5 +21,54 @@ public class AppFacade extends AbstractFacade<App> {
 
   public AppFacade() {
     super(App.class);
+  }
+
+  @RolesAllowed("adm-admin")
+  public void addApp(String name, String docUrl) throws UserFriendlyException {
+    if (name == null || name.isEmpty()) {
+      throw new UserFriendlyException("Name cannot be empty");
+    }
+
+    App app = new App(name, docUrl);
+
+    create(app);
+  }
+
+  @RolesAllowed({"adm-admin"})
+  public void editApp(
+          BigInteger appId,
+          String name,
+          String docUrl)
+          throws UserFriendlyException {
+
+    if (appId == null) {
+      throw new UserFriendlyException("appId cannot be empty");
+    }
+
+    App app = find(appId);
+
+    if (app == null) {
+      throw new UserFriendlyException("app not found with id: " + appId);
+    }
+
+    app.setName(name);
+    app.setDocUrl(docUrl);
+
+    edit(app);
+  }
+
+  @RolesAllowed("adm-admin")
+  public void removeSoftware(BigInteger appId) throws UserFriendlyException {
+    if (appId == null) {
+      throw new UserFriendlyException("appId cannot be empty");
+    }
+
+    App app = find(appId);
+
+    if (app == null) {
+      throw new UserFriendlyException("App not found with id: " + appId);
+    }
+
+    remove(app);
   }
 }
--- src/main/java/org/jlab/adm/persistence/entity/App.java
+++ src/main/java/org/jlab/adm/persistence/entity/App.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.io.Serializable;
-import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Objects;
 
 @Entity
@@ -13,10 +13,12 @@ public class App implements Serializable {
   private static final long serialVersionUID = 1L;
 
   @Id
+  @SequenceGenerator(name = "AppId", sequenceName = "APP_ID", allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "AppId")
   @Basic(optional = false)
   @NotNull
   @Column(name = "APP_ID", nullable = false, precision = 22, scale = 0)
-  private BigDecimal appId;
+  private BigInteger appId;
 
   @Basic(optional = false)
   @NotNull
@@ -28,11 +30,11 @@ public class App implements Serializable {
   @Column(name = "DOC_URL", nullable = true, length = 512)
   private String docUrl;
 
-  public BigDecimal getAppId() {
+  public BigInteger getAppId() {
     return appId;
   }
 
-  public void setAppId(BigDecimal appId) {
+  public void setAppId(BigInteger appId) {
     this.appId = appId;
   }
 
@@ -49,6 +51,13 @@ public class App implements Serializable {
   }
 
   public void setDocUrl(String docUrl) {
+    this.docUrl = docUrl;
+  }
+
+  public App(){}
+
+  public App(String name, String docUrl) {
+    this.name = name;
     this.docUrl = docUrl;
   }
 
